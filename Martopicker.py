a6954f13cfe87405a7d408bc7e3efa2cbd526981
thornydre <thornydre@gmail.com>
2024-02-20 12:31:25 +0100

    Update Martopicker.py

--- Martopicker.py
+++ Martopicker.py
@@ -19,7 +19,6 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 		self.connectInterface()
 		self.maya_job = cmds.scriptJob(event=["SelectionChanged", self.editor.selectionFromViewport])
 
-
 	def setInterface(self):
 		main_layout = QtWidgets.QVBoxLayout()
 
@@ -65,7 +64,6 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 
 		self.setLayout(main_layout)
 
-
 	def connectInterface(self):
 		self.mode_button.clicked.connect(self.toggleEditModeCommand)
 		self.color_button.clicked.connect(self.chooseColorCommand)
@@ -74,11 +72,9 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 		self.add_scripted_button.clicked.connect(self.textEditorCommand)
 		self.bg_image_button.clicked.connect(self.changeBackgroundCommand)
 
-
 	def toggleEditModeCommand(self):
 		self.editor.toggleEditMode()
 		self.edit_buttons_widget.setVisible(self.editor.getEditMode())
-
 
 	def chooseColorCommand(self):
 		color = QtGui.QColor(128, 128, 128)
@@ -89,7 +85,7 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 			color = selected_list[-1].getColor()
 
 		color_dialog = ColorPickerWindow(color, self)
-		color_dialog.move(self.pos().x() - color_dialog.width() * 2.5, self.pos().y())
+		# color_dialog.move(self.pos().x() - color_dialog.width() * 2.5, self.pos().y())
 		color_dialog.exec()
 
 		color_info = color_dialog.getData()
@@ -97,18 +93,14 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 		if color_info:
 			self.editor.setButtonColor(color_info["color"])
 
-
 	def sizeSliderCommand(self):
 		size = self.size_slider.value() / 5
-
 		self.editor.setButtonSizeOffset(size)
-
 
 	def buttonNameChangedCommand(self):
 		name = self.name_textfield.text()
 
 		self.editor.setButtonName(name)
-
 
 	def textEditorCommand(self):
 		text_editor = TextEditor(self)
@@ -120,16 +112,13 @@ class Martopicker(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 			self.editor.addEditorButton((50, 50), (20, 20), [], "rect", QtGui.QColor(255, 249, 23), "", button_info["script"])
 			self.editor.repaint()
 
-
 	def changeBackgroundCommand(self):
 		image_path = QtWidgets.QFileDialog.getOpenFileName(caption="Load background image", filter="Images (*.png *.xpm *.jpg)")[0]
 
 		self.editor.setBackgroundImage(image_path)
 
-
-	def dockCloseEventTriggered(self, e):
+	def dockCloseEventTriggered(self):
 		cmds.scriptJob(kill=self.maya_job)
-
 
 	def keyPressEvent(self, e):
 		self.editor.keyPressEvent(e)
@@ -158,7 +147,6 @@ class Editor(QtWidgets.QWidget):
 		self.resize(width, height)
 
 		self.bg_image = ""
-
 
 	def mousePressEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
@@ -198,7 +186,6 @@ class Editor(QtWidgets.QWidget):
 			if start_box:
 				self.box_selection[0] = e.x()
 				self.box_selection[1] = e.y()
-
 
 	def mouseReleaseEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
@@ -240,7 +227,6 @@ class Editor(QtWidgets.QWidget):
 			self.parent.name_textfield.setText("")
 			self.parent.name_textfield.setEnabled(False)
 
-
 	def mouseMoveEvent(self, e):
 		repaint = False
 
@@ -259,7 +245,6 @@ class Editor(QtWidgets.QWidget):
 
 		if repaint:
 			self.repaint()
-
 
 	def keyPressEvent(self, e):
 		if e.key() == QtCore.Qt.Key_Delete:
@@ -310,11 +295,9 @@ class Editor(QtWidgets.QWidget):
 
 				self.loadPicker(file_path)
 
-
 	def toggleEditMode(self):
 		self.edit_mode = not self.edit_mode
 		self.repaint()
-
 
 	def paintEvent(self, e):
 		qp = QtGui.QPainter()
@@ -334,7 +317,6 @@ class Editor(QtWidgets.QWidget):
 
 		qp.end()
 
-
 	def updateSelectMode(self, e):
 		select = []
 
@@ -353,7 +335,6 @@ class Editor(QtWidgets.QWidget):
 			cmds.select(select)
 
 		self.repaint()
-
 
 	def boxSelect(self):
 		select = []
@@ -377,7 +358,6 @@ class Editor(QtWidgets.QWidget):
 		if not self.edit_mode:
 			cmds.select(select)
 
-
 	def selectionFromViewport(self):
 		if not self.edit_mode:
 			viewport_selection = cmds.ls(sl=True)
@@ -396,10 +376,8 @@ class Editor(QtWidgets.QWidget):
 
 			self.repaint()
 
-
 	def getEditMode(self):
 		return self.edit_mode
-
 
 	def getSelectedList(self):
 		return self.selected_list
@@ -409,17 +387,14 @@ class Editor(QtWidgets.QWidget):
 			button.setColor(color)
 		self.repaint()
 
-
 	def setButtonSizeOffset(self, size):
 		for button in self.selected_list:
 			button.setSize(size)
 		self.repaint()
 
-
 	def setButtonName(self, name):
 		self.selected_list[0].setText(name)
 		self.repaint()
-
 
 	def selectButton(self, button):
 		if not self.edit_mode and button.getScript():
@@ -429,12 +404,10 @@ class Editor(QtWidgets.QWidget):
 			if button not in self.selected_list:
 				self.selected_list.append(button)
 
-
 	def deselectButton(self, button):
 		button.deselect()
 		if button in self.selected_list:
 			self.selected_list.remove(button)
-
 
 	def setBackgroundImage(self, image_path):
 		self.bg_image = image_path
@@ -443,14 +416,12 @@ class Editor(QtWidgets.QWidget):
 		self.setMinimumSize(self.bg_scaled_pixmap.width(), self.bg_scaled_pixmap.height())
 		self.resize(self.bg_scaled_pixmap.width(), self.bg_scaled_pixmap.height())
 
-
 	def savePicker(self, path):
 		if path:
 			if os.path.splitext(path)[1] == ".pik":
 				data = {"buttons": self.buttons_list, "background": self.bg_image}
 				with open(path, "wb") as file:
 					pickle.dump(data, file)
-
 
 	def loadPicker(self, path):
 		if path:
@@ -460,7 +431,6 @@ class Editor(QtWidgets.QWidget):
 					self.buttons_list = data["buttons"]
 					self.setBackgroundImage(data["background"])
 					self.repaint()
-
 
 	def generateButtonColor(self, selection):
 		name_based = False
@@ -481,10 +451,8 @@ class Editor(QtWidgets.QWidget):
 
 		return QtGui.QColor(255, 249, 23)
 
-
 	def addEditorButton(self, pos, size, elem, shape, color, text, script):
 		self.buttons_list.append(EditorButton(pos[0], pos[1], size[0], size[1], elem, shape, color, text, script))
-
 
 	def verticalAlignMin(self):
 		min_button = self.selected_list[0]
@@ -498,7 +466,6 @@ class Editor(QtWidgets.QWidget):
 
 		self.repaint()
 
-
 	def verticalAlignMax(self):
 		min_button = self.selected_list[0]
 
@@ -510,7 +477,6 @@ class Editor(QtWidgets.QWidget):
 			button.setPosX(min_button.getPosX())
 
 		self.repaint()
-
 
 	def horizontalAlignMin(self):
 		min_button = self.selected_list[0]
@@ -524,7 +490,6 @@ class Editor(QtWidgets.QWidget):
 
 		self.repaint()
 
-
 	def horizontalAlignMax(self):
 		min_button = self.selected_list[0]
 
@@ -536,7 +501,6 @@ class Editor(QtWidgets.QWidget):
 			button.setPosY(min_button.getPosY())
 
 		self.repaint()
-
 
 	def align(self):
 		start_button = self.selected_list[-1]
@@ -574,59 +538,45 @@ class EditorButton():
 
 		self.selected = False
 
-
 	def getPosX(self):
 		return self.pos_x
-
 
 	def getPosY(self):
 		return self.pos_y
 
-
 	def setPosX(self, pos_x):
 		self.pos_x = pos_x
-
 
 	def setPosY(self, pos_y):
 		self.pos_y = pos_y
 
-
 	def getRadiusX(self):
 		return self.radius_x
-
 
 	def getRadiusY(self):
 		return self.radius_y
 
-
 	def setEditOffset(self, edit_offset):
 		self.edit_offset = edit_offset
-
 
 	def getEditOffset(self):
 		return self.edit_offset
 
-
 	def getSelection(self):
 		return self.selection
-
 
 	def getShape(self):
 		return self.shape
 
-
 	def getColor(self):
 		return self.color
-
 
 	def setColor(self, color):
 		self.color = color
 		self.selected_color = QtGui.QColor.fromHsv(self.color.hue(), max(self.color.saturation() - 120, 0), min(self.color.value() + 120, 255))
 
-
 	def getText(self):
 		return self.text
-
 
 	def setText(self, text):
 		self.text = text
@@ -644,14 +594,11 @@ class EditorButton():
 			self.radius_x = self.default_radius_x + self.size_offset
 			self.radius_y = self.default_radius_y + self.size_offset
 
-
 	def getScript(self):
 		return self.script
 
-
 	def getSize(self):
 		return self.size_offset
-
 
 	def setSize(self, size):
 		self.size_offset = size
@@ -669,22 +616,17 @@ class EditorButton():
 			self.radius_x = self.default_radius_x + self.size_offset
 			self.radius_y = self.default_radius_y + self.size_offset
 
-
 	def select(self):
 		self.selected = True
-
 
 	def deselect(self):
 		self.selected = False
 
-
 	def executeScript(self):
 		exec(self.script)
 
-
 	def getSelected(self):
 		return self.selected
-
 
 	def draw(self, qp, edit_mode):
 		qp.setBrush(self.color)
@@ -711,7 +653,6 @@ class EditorButton():
 			if self.text:
 				qp.drawText(self.pos_x - size_x/2, self.pos_y - size_y/2, size_x, size_y, QtCore.Qt.AlignVCenter|QtCore.Qt.AlignHCenter, self.text)
 
-
 	def isOnButton(self, x, y):
 		if x > self.pos_x - self.radius_x/2:
 			if x < self.pos_x + self.radius_x/2:
@@ -730,7 +671,6 @@ class TextEditor(QtWidgets.QDialog):
 
 		self.setInterface()
 		self.connectInterface()
-
 
 	def setInterface(self):
 		main_layout = QtWidgets.QVBoxLayout()
@@ -751,11 +691,9 @@ class TextEditor(QtWidgets.QDialog):
 
 		self.setLayout(main_layout)
 
-
 	def connectInterface(self):
 		self.submit_button.clicked.connect(self.createButtonCommand)
 		self.cancel_button.clicked.connect(self.cancelCommand)
-
 
 	def createButtonCommand(self):
 		self.script = self.text_editor.toPlainText()
@@ -763,11 +701,9 @@ class TextEditor(QtWidgets.QDialog):
 		if self.script:
 			self.close()
 
-
 	def cancelCommand(self):
 		self.validate = False
 		self.close()
-
 
 	def getData(self):
 		if self.validate:
@@ -784,10 +720,9 @@ class ColorPickerWindow(QtWidgets.QDialog):
 
 		self.color = color
 		self.validate = True
-		
+
 		self.setInterface()
 		self.connectInterface()
-
 
 	def setInterface(self):
 		vertical_layout = QtWidgets.QVBoxLayout()
@@ -815,37 +750,30 @@ class ColorPickerWindow(QtWidgets.QDialog):
 
 		self.setLayout(vertical_layout)
 
-
 	def connectInterface(self):
 		self.color_wheel_widget.color_changed_signal.connect(self.colorChangedCommand)
 		self.color_value_widget.value_changed_signal.connect(self.valueChangedCommand)
 		self.submit_button.clicked.connect(self.submitCommand)
 		self.cancel_button.clicked.connect(self.cancelCommand)
 
-
 	def getColor(self):
 		return self.color_wheel_widget.getColor()
-
 
 	@QtCore.Slot()
 	def valueChangedCommand(self):
 		self.color_wheel_widget.setValue(self.color_value_widget.getValue())
 		self.colorChangedCommand()
 
-
 	@QtCore.Slot()
 	def colorChangedCommand(self):
 		self.color_display_widget.setColor(self.color_wheel_widget.getColor())
 
-
 	def submitCommand(self):
 		self.close()
-
 
 	def cancelCommand(self):
 		self.validate = False
 		self.close()
-
 
 	def getData(self):
 		if self.validate:
@@ -877,7 +805,6 @@ class ColorWheel(QtWidgets.QWidget):
 
 		self.qp = QtGui.QPainter()
 
-
 	def paintEvent(self, e):
 		self.qp.begin(self)
 		self.qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
@@ -904,7 +831,6 @@ class ColorWheel(QtWidgets.QWidget):
 
 		self.qp.end()
 
-
 	def mousePressEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
 			self.setFocus()
@@ -915,20 +841,17 @@ class ColorWheel(QtWidgets.QWidget):
 
 			self.repaint()
 
-
 	def mouseMoveEvent(self, e):
 		if self.pressed:
 			self.updateCursorPos(e.x(), e.y())
 
 			self.repaint()
 
-
 	def mouseReleaseEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
 			self.pressed = False
 
 			self.repaint()
-
 
 	def updateCursorPos(self, new_x, new_y):
 		self.cursor_pos = [new_x, new_y]
@@ -953,25 +876,20 @@ class ColorWheel(QtWidgets.QWidget):
 
 		self.color_changed_signal.emit()
 
-
 	def setHue(self, hue):
 		self.hue = hue
 		self.repaint()
-
 
 	def setSaturation(self, saturation):
 		self.saturation = saturation
 		self.repaint()
 
-
 	def setValue(self, value):
 		self.value = value
 		self.repaint()
 
-
 	def getColor(self):
 		return QtGui.QColor.fromHsvF(self.hue, self.saturation, self.value)
-
 
 	def positionCursor(self):
 		x_coord = math.sin(self.hue * math.pi * 2) * self.radius * self.saturation * -1
@@ -1002,7 +920,6 @@ class ColorValue(QtWidgets.QWidget):
 
 		self.qp = QtGui.QPainter()
 
-
 	def paintEvent(self, e):
 		self.qp.begin(self)
 		self.qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
@@ -1022,7 +939,6 @@ class ColorValue(QtWidgets.QWidget):
 
 		self.qp.end()
 
-
 	def mousePressEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
 			self.setFocus()
@@ -1033,20 +949,17 @@ class ColorValue(QtWidgets.QWidget):
 
 			self.repaint()
 
-
 	def mouseMoveEvent(self, e):
 		if self.pressed:
 			self.updateCursorPos(e.x(), e.y())
 
 			self.repaint()
 
-
 	def mouseReleaseEvent(self, e):
 		if e.button() == QtCore.Qt.MouseButton.LeftButton:
 			self.pressed = False
 
 			self.repaint()
-
 
 	def updateCursorPos(self, new_x, new_y):
 		self.cursor_pos = [self.width / 2, new_y]
@@ -1058,14 +971,11 @@ class ColorValue(QtWidgets.QWidget):
 
 		self.value_changed_signal.emit()
 
-
 	def setValue(self, value):
 		self.value = value
 
-
 	def getValue(self):
 		return self.value
-
 
 	def positionCursor(self):
 		self.cursor_pos[1] = self.height * (1 - self.value)
@@ -1085,7 +995,6 @@ class ColorDisplay(QtWidgets.QWidget):
 
 		self.qp = QtGui.QPainter()
 
-
 	def paintEvent(self, e):
 		self.qp.begin(self)
 		self.qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
@@ -1096,7 +1005,6 @@ class ColorDisplay(QtWidgets.QWidget):
 		self.qp.drawRoundedRect(display_rect, 5, 5)
 
 		self.qp.end()
-
 
 	def setColor(self, color):
 		self.color = color
